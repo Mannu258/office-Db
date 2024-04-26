93f7a14ceada5782c4bdbd01e09c5a3479ce65fa
Mandeep <mandeep_mis@mojopanda.com>
2024-04-26 14:58:27 +0530

    fix admin panel post method not working

--- dataapp/admin.py
+++ dataapp/admin.py
@@ -4,25 +4,25 @@ from . models import *
 
 # Register your models here.
 
-class mojoAdminForm(forms.ModelForm):
-    list_display = ['Item_Code','Item_name']
-    search_fields = ['Item_Code', 'Item_name']
-    class Meta:
-        model = mojo
-        fields = '__all__'
+# class mojoAdminForm(forms.ModelForm):
+#     list_display = ['Item_Code','Item_name']
+#     search_fields = ['Item_Code', 'Item_name']
+#     class Meta:
+#         model = mojo
+#         fields = '__all__'
 
-    def clean(self):
-        cleaned_data = super().clean()
-        field_to_check = cleaned_data.get('Item_Code')
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         field_to_check = cleaned_data.get('Item_Code')
 
-        # Check if the value already exists in the database
-        if mojo.objects.filter(Item_Code=field_to_check).exists():
-            raise forms.ValidationError("This value already exists.")
+#         # Check if the value already exists in the database
+#         if mojo.objects.filter(Item_Code=field_to_check).exists():
+#             raise forms.ValidationError("This value already exists.")
 
-        return cleaned_data
+#         return cleaned_data
 
 class MojoAdmin(admin.ModelAdmin):
-    form = mojoAdminForm
+    # form = mojoAdminForm
     list_display = ['Item_Code','Item_name']
     search_fields = ['Item_Code', 'Item_name']
 
