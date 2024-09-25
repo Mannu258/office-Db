83607edb8a30a02735d17730fc457abf2e187963
Your Name <you@example.com>
2024-09-25 17:01:22 +0530

    fix pagination

--- dataapp/views.py
+++ dataapp/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from . models import *
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
-    params = mojo.objects.all().order_by('Item_Code')
     try:
-        if request.method=="POST":
+        if request.method == "POST":
             code = request.POST.get('code')
-            # print(code)
-            params = mojo.objects.filter(Q(Item_Code__icontains=code) | Q(Item_name__icontains=code))
+            params = mojo.objects.filter(Q(Item_Code__icontains=code) | Q(Item_name__icontains=code)).order_by('Item_Code')
+        else:
+            params = mojo.objects.all().order_by('Item_Code')
     except:
         params = mojo.objects.all().order_by('Item_Code')
+    # Pagination
+    paginator = Paginator(params, 25)  # Show 25 items per page.
+    page_number = request.GET.get("page")
+    params = paginator.get_page(page_number)
 
-    return render(request,'index.html',{'params':params})
+    return render(request, "index.html", {"params": params})
 
 def details(request,id):
     params = mojo.objects.filter(id=id)
